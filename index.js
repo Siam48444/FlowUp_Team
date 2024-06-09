d2ea82b77e341e965b76990d800b2ef5fa7f1344
Siam <siam48444@gmail.com>
2024-06-09 18:56:42 +0600

    Main

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// // After loading animations.
-// gsap.from("nav", {
-//     y: "-30%",
-//     duration: 1,
-// });
-// gsap.from(".hero_image_container", {
-//     y: "10%",
-//     opacity: 0,
-//     duration: 1,
-// });
+// After loading animations.
+gsap.from("nav", {
+    y: "-30%",
+    duration: 1,
+});
+gsap.from(".hero_image_container", {
+    y: "10%",
+    opacity: 0,
+    duration: 1,
+});
 
-// // Appearing on scroll animation.
-// appear_on_scroll_elements = document.querySelectorAll(".appear_on_scroll");
-// appear_on_scroll_elements.forEach((scroll_element) => {
-//     gsap.from(scroll_element, {
-//         opacity: 0,
-//         duration: 0.6,
-//         y: "80",
-//         scrollTrigger: {
-//             trigger: scroll_element,
-//             start: "top 70%",
-//         },
-//     });
-// });
+// Appearing on scroll animation.
+appear_on_scroll_elements = document.querySelectorAll(".appear_on_scroll");
+appear_on_scroll_elements.forEach((scroll_element) => {
+    gsap.from(scroll_element, {
+        opacity: 0,
+        duration: 0.6,
+        y: "80",
+        scrollTrigger: {
+            trigger: scroll_element,
+            start: "top 70%",
+        },
+    });
+});
