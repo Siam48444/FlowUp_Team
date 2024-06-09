e0a7de354191102cf379523830804a9391a6d6ee
Siam <siam48444@gmail.com>
2024-06-09 13:06:23 +0600

    Main

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// After loading animations.
 gsap.from("nav", {
     y: "-30%",
     duration: 1,
@@ -6,4 +7,18 @@ gsap.from(".hero_image_container", {
     y: "10%",
     opacity: 0,
     duration: 1,
+});
+
+// Appearing on scroll animation.
+appear_on_scroll_elements = document.querySelectorAll(".appear_on_scroll");
+appear_on_scroll_elements.forEach((scroll_element) => {
+    gsap.from(scroll_element, {
+        opacity: 0,
+        duration: 0.8,
+        y: "80",
+        scrollTrigger: {
+            trigger: scroll_element,
+            start: "top 70%",
+        },
+    });
 });
