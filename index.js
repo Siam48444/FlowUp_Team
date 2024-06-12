f1e579b7faf55ac75f43032b756326905afc4eee
Siam <siam48444@gmail.com>
2024-06-12 17:32:42 +0600

    Main

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ gsap.from(".hero_container", {
 });
 
 // Frequently asked questions.
+const questions = document.querySelectorAll(".questions");
+const answers = document.querySelectorAll(".question_answer");
+const question_svg = document.querySelectorAll(".question_main svg");
+
+for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", () => {
+        answers[i].classList.toggle("answer_open");
+        question_svg[i].classList.toggle("svg_rotated");
+    });
+}
