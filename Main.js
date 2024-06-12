6fb4bd20cb9caadd10c0f30acbb713d3eefefb12
Siam <siam48444@gmail.com>
2024-06-12 18:33:30 +0600

    Main

--- Main.js
+++ Main.js
@@ -32,9 +32,9 @@ function scrollingdUp() {
 footer = document.querySelector("footer");
 gsap.from(footer, {
     opacity: 0,
-    duration: 1,
+    duration: 1.5,
     scrollTrigger: {
         trigger: footer,
-        start: "top 95%",
+        start: "top 100%",
     },
 });
